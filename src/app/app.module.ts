5ebd9dcb76b3695cbf5e6c8f32d7514fbdc99551
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-10 21:02:09 +1100

    Used alpha-3 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
-import { MenuModule } from 'ng2-fan-menu';
+import { FanMenuModule } from 'ng2-fan-menu';
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        MenuModule
+        FanMenuModule,
     ],
     declarations: [
         AppComponent
